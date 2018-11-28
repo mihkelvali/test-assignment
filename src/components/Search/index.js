676f489907a449587ba5f2d54cfc49f557fba502
Mihkel Väli <mihkel.vali@mooncascade.com>
2018-11-28 22:18:25 +0200

    some working stuff

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,50 +1,90 @@
-import React, {Component} from 'react';
-import pin from '../../icons/ic-pin.svg';
-import './styles.css';
-import Suggestions from '../Suggestions';
+import React, { Component } from "react";
+import pin from "../../icons/ic-pin.svg";
+import "./styles.css";
+import Suggestions from "../Suggestions";
+import DisplayWeather from "../DisplayWeather";
 
 class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            weatherSuggestions: [],
-            query: '',
-        }
+            query: "",
+            locations: [],
+            woeId: "",
+            detailInfo: []
+        };
     }
 
-    getData = () => {
-        const apiCall = 'http://internship-proxy.aw.ee:3001/location?query=' + this.state.query;
-        fetch(apiCall).then(response => response.json())
+    fetchLocations = () => {
+        const url =
+            "http://internship-proxy.aw.ee:3001/location?query=" +
+            this.state.query;
+        fetch(url)
+            .then(response => response.json())
             .then(data => {
-            this.setState ({
-                weatherSuggestions: data,
-            })
-        });
-    };
-
-    handleOnKeyUp = () => {
-            this.setState({query: this.refs.search.value}, () => {
-                this.getData();
+                this.setState({
+                    locations: data
+                });
             });
     };
 
+    handleOnKeyUp = () => {
+        this.setState({ query: this.refs.search.value }, () => {
+            this.fetchLocations();
+        });
+    };
+
     handleFocus = () => {
-        this.refs.pTag.style.display = 'block';
+        this.refs.pTag.style.display = "block";
+        this.setState({
+            detailInfo: [],
+            query: ""
+        });
+    };
+
+    selectLocation = ({ target: { id: woeId } }) => {
+        this.setState({
+            woeId
+        });
+        this.fetchDetailedInformation();
+    };
+
+    fetchDetailedInformation = () => {
+        fetch("http://internship-proxy.aw.ee:3001/location/" + this.state.woeId)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    locations: [],
+                    detailInfo: data.consolidated_weather
+                });
+            });
     };
 
     render() {
+        const { locations, detailInfo } = this.state;
         return (
             <React.Fragment>
                 <div className="center">
-                    <img src={pin} alt="icon"/>
+                    <img src={pin} alt="icon" />
                     <p ref="pTag">Search location...</p>
-                    <input type="search" ref="search"  placeholder="Search location.." onFocus={this.handleFocus} onKeyUp={this.handleOnKeyUp}/>
-                    <Suggestions weatherInfo={this.state.weatherSuggestions}/>
+                    <input
+                        type="search"
+                        ref="search"
+                        placeholder="Search location.."
+                        onFocus={this.handleFocus}
+                        onKeyUp={this.handleOnKeyUp}
+                    />
+                    {locations && (
+                        <Suggestions
+                            weatherInfo={locations}
+                            selectLocation={this.selectLocation}
+                        />
+                    )}
+                    {detailInfo && <DisplayWeather locationInfo={detailInfo} />}
                 </div>
             </React.Fragment>
         );
     }
 }
-
 
 export default Search;
--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,56 +1,20 @@
-import React, {Component} from 'react';
-import './styles.css';
-import DisplayWeather from '../DisplayWeather';
+import React from "react";
+import "./styles.css";
 
-class Suggestions extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            weatherInfo: [],
-            locationData: [],
-            woeid: '',
-        }
-    }
-
-    getWeatherData = () => {
-        const apiCall = 'http://internship-proxy.aw.ee:3001/location/' + this.state.woeid;
-        fetch(apiCall).then(response => response.json())
-            .then(data => {
-            this.setState ({
-                locationData: data.consolidated_weather
-            })
-        });
-    };
-
-    onClick = (e) => {
-        const locationId = e.target.id;
-        this.setState({woeid: locationId}, () => {
-            this.getWeatherData();
-        });
-        this.refs.locationsList.style.display = 'none';
-    };
-
-    render() {
-        if (this.props.weatherInfo.length === 0) {
-            return null;
-        }
-        else {
-            return (
-                <div>
-                    <ul className="suggestions" ref='locationsList'>
-                        {this.props.weatherInfo.map(data => (
-                            <li onClick={this.onClick} key={data.woeid} id={data.woeid}>
-                                {data.title}
-                            </li>
-                        ))}
-                    </ul>
-                    <DisplayWeather locationInfo={this.state.locationData}/>
-                </div>
-            );
-        }
-
-    }
-}
-
+const Suggestions = ({ weatherInfo, selectLocation }) => (
+    <div>
+        <ul className="suggestions">
+            {weatherInfo.map(data => (
+                <li
+                    onClick={selectLocation}
+                    key={data.woeid}
+                    id={data.woeid}
+                >
+                    {data.title}
+                </li>
+            ))}
+        </ul>
+    </div>
+);
 
 export default Suggestions;
